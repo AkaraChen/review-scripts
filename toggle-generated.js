61d4091e17b858fc9f285d46b822df997b984d0b
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-10-10 15:21:43 +0800

    docs: add

--- toggle-generated.js
+++ toggle-generated.js
@@ -1,3 +1,11 @@
+/*
+Use this script in GitHub PR review page to toggle files contains keywords in the file content.
+This is useful when you have a generated file in your PR and you want to hide it from review.
+
+这个脚本可以在 GitHub PR review 页面中，根据文件内容中是否包含关键字来切换文件的显示状态。
+当你的 PR 中包含有生成的文件，而你不想在 review 中看到它时，这个脚本就很有用了。
+*/
+
 const keywords = [
 	'generated',
 	'do not edit',
